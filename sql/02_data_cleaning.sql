e4fc50d25b0d9956a2a5366e5c8f22b27b1bf95b
wuyue6756-oss <wuyue6756@gmail.com>
2026-03-23 17:22:51 +0800

    Update 02_data_cleaning.sql

---
+++ sql/02_data_cleaning.sql
@@ -0,0 +1,11 @@
+-- 清洗后视图：去掉明显异常值
+CREATE VIEW vw_trip_clean AS
+SELECT *
+FROM fact_trip
+WHERE trip_distance > 0
+  AND total_amount > 0
+  AND fare_amount >= 0
+  AND PULocationID IS NOT NULL
+  AND DOLocationID IS NOT NULL
+  AND tpep_pickup_datetime IS NOT NULL
+  AND tpep_dropoff_datetime IS NOT NULL;
